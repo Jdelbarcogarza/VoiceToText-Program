8d33ac7829557ca15e609366801763a7623fb749
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-01-27 19:31:53 -0600

    the program now accepts utf-8 characters

--- API_WEB/RESTAPI/controllers.py
+++ API_WEB/RESTAPI/controllers.py
@@ -13,15 +13,20 @@ def c_txt():
     phrase = listen_speech()
     write_on_file(phrase)
 
-    text = open(getcwd() +'\\API_WEB\\RESTAPI\\frases.txt','r')
+    text = open(getcwd() +'\\API_WEB\\RESTAPI\\frases.txt','r', encoding='utf-8')
+    
+    # se colocan todas las palabras dentro del archivo en un diccionario
     phrases ={}
     key = 0
     for i in text:
         
         phrases[str(key)] = i
         key += 1
-    text.close()    
+    text.close()
+    
+    # aqui se imprime el diccionario.
     print(phrases)
+
     json_phrases = jsonify(phrases)
     #json_phrases.headers.add('Access-Control-Allow-Origin', '*')
     return json_phrases
