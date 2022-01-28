8a3924a9272382afa777f95f700cf0e3b9c85a7a
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-01-27 18:38:24 -0600

    implemented speech to text functionality

--- API_WEB/RESTAPI/controllers.py
+++ API_WEB/RESTAPI/controllers.py
@@ -1,12 +1,18 @@
 
 from flask import jsonify
 from os import getcwd
+from reconocimiento_frases import *
+
 
 def c_print():
     return "LOREM EPSILUM"
 
 #Ruta('txt')
 def c_txt():
+
+    phrase = listen_speech()
+    write_on_file(phrase)
+
     text = open(getcwd() +'\\API_WEB\\RESTAPI\\frases.txt','r')
     phrases ={}
     key = 0
@@ -19,3 +25,6 @@ def c_txt():
     json_phrases = jsonify(phrases)
     #json_phrases.headers.add('Access-Control-Allow-Origin', '*')
     return json_phrases
+
+if __name__ == "__main__":
+    print("Este programa no debe correrse directamente.")
