f840399d0380b7bdc62e477fe33a116747f78266
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-01-29 16:17:08 -0600

    optimized code and declared global variables

--- API_WEB/RESTAPI/main.py
+++ API_WEB/RESTAPI/main.py
@@ -1,23 +1,27 @@
 
-from flask import Flask,jsonify
+from flask import Flask, render_template
 from flask_cors import CORS
 from controllers import *
-from os import getcwd
 
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/test')
-def GET_TEST():
-    return c_print()
+# 
+global phraseID
+phraseID = 0
 
 @app.route('/txt')
 def GET_TXT():
-    return c_txt()
+
+    global phraseID
+    phraseID = phraseID + 1
+    print(phraseID)
+
+    return c_txt(phraseID)
 
 @app.route('/')
-def GET_CWD():
-    return "ESTO ES EL BACKEND Escribe en la ruta 'txt' o 'test'"
+def index():
+    return render_template("index.html")
 
 app.run(debug = True)
