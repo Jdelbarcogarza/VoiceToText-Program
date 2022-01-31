7f6091ec869df3d8aaaa8872e205442130ec81b2
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-01-31 16:44:33 -0600

    implemented error recognition

--- API_WEB/RESTAPI/controllers.py
+++ API_WEB/RESTAPI/controllers.py
@@ -11,8 +11,18 @@ def c_print():
 def c_txt(phraseID):
 
     phrase = listen_speech()
+
+    # value is an error then just don't send a dictionary value
+    if (phrase == 'repeat'):
+        # TODO: this message should be read by the JS code and handle it accordingly.
+        # right now there is an uncaught promise due to a syntax error con the js code because
+        # no json object is being sent.
+        return 'error'
+
+    # write phrase on file once it is validated.
     write_on_file(phrase)
 
+    # return json object
     return jsonify({phraseID: phrase})
 
 if __name__ == "__main__":
